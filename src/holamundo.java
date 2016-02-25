cf9d7e2300591a97e742328bc6d32d1268cc5ba8
Daniel Perez <dperez@elpuig.xeill.net>
2016-02-25 17:13:05 +0100

    002 prueba

--- src/holamundo.java
+++ src/holamundo.java
@@ -4,6 +4,7 @@ public class holamundo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hola mundo");
+		System.out.println("hola mundo 2");
 	}
 
 }
